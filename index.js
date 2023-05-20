2276297393266c7da2eeeefbaca40fb36ea822f3
Harish Kanagal <78932555+Concord1@users.noreply.github.com>
2023-05-20 19:44:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 function getAccel(){
-  // Check for mobile user
-  var mobileUser = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  var permis = 'granted';
-  if(mobileUser){
-     DeviceMotionEvent.requestPermission().then(response => {
-     permis = response;
-    });
-  }                                
-  if (permis == 'granted') {
-    window.addEventListener('deviceorientation',(event) => {
-        x = event.alpha;
-        y = event.beta;
-        z = event.gamma;
-        var xElement = document.getElementById("x");
-        var yElement = document.getElementById("y");
-        var zElement = document.getElementById("z");
-        xElement.textContent = "X: " + x.toFixed(2);
-        yElement.textContent = "Y: " + y.toFixed(2);
-        zElement.textContent = "Z: " + z.toFixed(2);
-     });
-   }
+    const redis = require('redis')
+    const redisClient = redis.createClient({
+      host: "containers-us-west-12.railway.app",
+      port: 8001,
+      password: "TSnu2tKQb8hj5Ln7trXe"
+    })
 
+    redisClient.on('error', err => {
+      console.log('Redis err')
+      console.log(err)
+    })
+
+    redisClient.set("Hello", "World")
+
+
+    // Check for mobile user
+    var mobileUser = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    var permis = 'granted';
+    if(mobileUser){
+       DeviceMotionEvent.requestPermission().then(response => {
+       permis = response;
+      });
+    }                                
+    if (permis == 'granted') {
+      window.addEventListener('deviceorientation',(event) => {
+          x = event.alpha;
+          y = event.beta;
+          z = event.gamma;
+          var xElement = document.getElementById("x");
+          var yElement = document.getElementById("y");
+          var zElement = document.getElementById("z");
+          xElement.textContent = "X: " + x.toFixed(2);
+          yElement.textContent = "Y: " + y.toFixed(2);
+          zElement.textContent = "Z: " + z.toFixed(2);
+       });
+     }
+  }
    
-}
- 
