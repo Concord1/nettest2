f01eaf0a7e52f24951a26f09c152088b3a2e7120
Concord1 <78932555+Concord1@users.noreply.github.com>
2023-05-20 00:08:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-// Import packages
-const express = require("express");
-const socketIO = require("socket.io");
-const path = require("path");
-
-// Configuration
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-// Start server
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
- .listen(PORT, () => console.log("Listening on localhost:" + PORT));
-
-// Initiatlize SocketIO
-const io = socketIO(server);
-
-// Register "connection" events to the WebSocket
-io.on("connection", function(socket) {
-  // Register "join" events, requested by a connected client
-  socket.on("join", function (room) {
-    // join channel provided by client
-    socket.join(room)
-    
-    // Register "image" events, sent by the client
-    socket.on("accel", function(msg) {
-        var title = msg.x;
-        console.log("Received image title:", title);
+function getAccel(){
+  // Check for mobile user
+  var mobileUser = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  var permis = 'granted';
+  if(mobileUser){
+     DeviceMotionEvent.requestPermission().then(response => {
+     permis = response;
     });
-  })
-});
+  }                                
+  if (permis == 'granted') {
+    window.addEventListener('deviceorientation',(event) => {
+        x = event.alpha;
+        y = event.beta;
+        z = event.gamma;
+        var xElement = document.getElementById("x");
+        var yElement = document.getElementById("y");
+        var zElement = document.getElementById("z");
+        xElement.textContent = "X: " + x.toFixed(2);
+        yElement.textContent = "Y: " + y.toFixed(2);
+        zElement.textContent = "Z: " + z.toFixed(2);
+     });
+   }
+
+   
+}
+ 
