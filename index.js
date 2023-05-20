ec2d3ff7a0b81065dbd0f5ff663d54dcb01a83c7
Concord1 <78932555+Concord1@users.noreply.github.com>
2023-05-19 22:42:39 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// Import packages
+const express = require("express");
+const socketIO = require("socket.io");
+const path = require("path");
+
+// Configuration
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+// Start server
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+ .listen(PORT, () => console.log("Listening on localhost:" + PORT));
+
+// Initiatlize SocketIO
+const io = socketIO(server);
+
+// Register "connection" events to the WebSocket
+io.on("connection", function(socket) {
+  // Register "join" events, requested by a connected client
+  socket.on("join", function (room) {
+    // join channel provided by client
+    socket.join(room)
+    
+    // Register "image" events, sent by the client
+    socket.on("accel", function(msg) {
+        var title = msg.x;
+        console.log("Received image title:", title);
+    });
+  })
+});
